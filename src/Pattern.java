126ce3cd7845d6d58d96b2a8953ae1ed9ee90983
Ryan Yovanda <ryanyovanda@gmail.com>
2024-09-05 16:27:59 +0700

    /Function: Add rest function

--- src/Pattern.java
+++ src/Pattern.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
         int n = scanner.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
